b723b00446cf0ba858e73e95ca625f9a555e8edd
Bruno Germano <bruno.germano@gmail.com>
2017-07-04 20:36:15 +0000

    add config file

---
+++ src/config.h
@@ -0,0 +1,28 @@
+//------- Configure aqui com as suas informações! ------
+
+/*
+  Dados do seu WIFI
+
+  ssid[] é o nome da sua rede wifi, escreva exatamente como é configurada, com maiúsculas e tudo.
+  password[] é a senha do seu wifi
+*/
+char ssid[] = "Nome da Rede Wifi"; // your network SSID (name)
+char password[] = "senha";   // your network password
+
+/*
+  google API key
+
+  API_KEY é a chave de acesso a api do youtube
+
+  para criar a sua, siga essas instruções: https://support.google.com/cloud/answer/6158862?hl=en
+*/
+#define API_KEY "sua api key"
+
+/*
+  youtube channel ID
+
+  CHANNEL_ID é o que identifica o seu canal no youtube
+
+  para encontrar o seu siga essas instruções: https://support.google.com/youtube/answer/3250431?hl=en
+*/
+#define CHANNEL_ID "seu channel id"
